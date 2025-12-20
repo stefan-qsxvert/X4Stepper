fb65e1566ced30c268ae1eb16ae58a607b0825fe
stefa <stefa@10.7.0.13>
2025-12-20 23:20:26 +0100

    1

---
+++ X4Stepper/Core/Src/App.cpp
@@ -0,0 +1,24 @@
+/*
+ * App.cpp
+ *
+ *  Created on: 20 gru 2025
+ *      Author: stefa
+ */
+
+#include <iostream>
+#include "App.hpp"
+
+//class App{
+
+//public:
+
+void App::prim(int i){
+	 std::cout << "test" << std::endl;
+}
+
+void App::sec(){
+//	HAL_
+
+}
+
+//};
---
+++ X4Stepper/Core/Src/App.hpp
@@ -0,0 +1,23 @@
+/*
+ * App.hpp
+ *
+ *  Created on: 20 gru 2025
+ *      Author: stefa
+ */
+
+#ifndef SRC_APP_HPP_
+#define SRC_APP_HPP_
+
+#endif /* SRC_APP_HPP_ */
+
+
+
+class App{
+
+public:
+
+void prim(int i);
+void sec();
+
+
+};
