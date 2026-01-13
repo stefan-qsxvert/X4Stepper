a185dd59e3072665b54f9375c224f34adf739ae8
stefa <stefa@Arek_q5q>
2026-01-13 20:00:28 +0100

    0

--- X4Stepper/Core/Src/App/App.cpp
+++ X4Stepper/Core/Src/App/App.cpp
@@ -1,1 +1,18 @@
+#include "Args.hpp"
+#include <iostream>
 
+class App{
+
+private:
+	Args args;
+public:
+	App(Args args_){
+		args = args_;
+	};
+
+void run(){
+
+};
+
+
+};
--- X4Stepper/Core/Src/App/Args.cpp
+++ X4Stepper/Core/Src/App/Args.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include "Args.hpp"
 
+
 	uint32_t Args::getADCWait(){
 		return ADCWait;
 	}
